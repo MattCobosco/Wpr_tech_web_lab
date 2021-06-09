e3311f52327c2ab020dd77f845b2da5b59155e56
MattCobosco <mateuszkalucki@gmail.com>
2021-06-09 22:01:04 +0200

    mobile done

---
+++ lab_4/scripts.js
@@ -0,0 +1,9 @@
+function showDropdown(){
+    var doc = document.getElementById("menu_dropdown");
+    if (doc.style.display === "none"){
+        doc.style.display = "block";
+    }
+    else {
+        doc.style.display = "none";
+    }
+}
